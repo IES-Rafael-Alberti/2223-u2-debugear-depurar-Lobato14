019110bec63db7ae7679691847e75a146bf800be
rubencelislobato@gmail.com <rubencelislobato@gmail.com>
2023-10-24 19:35:03 +0200

    Cambios realizados en el codigo del algoritmo burbuja y de su test

--- src/algoritmo_burbuja.py
+++ src/algoritmo_burbuja.py
@@ -1,1 +1,15 @@
-a = [8, 3, 1, 19, 14]
+def ordenacion_burbuja(lista):
+    longitud = len(lista)
+    for indice_externo in range(longitud):
+        for indice_interno in range(0, longitud - indice_externo - 1):
+            if lista[indice_interno] > lista[indice_interno + 1]:
+                lista[indice_interno], lista[indice_interno + 1] = lista[indice_interno + 1], lista[indice_interno]
+    return lista
+
+if __name__ == "__main__":
+    # Entrada
+    lista = [8, 3, 1, 19, 14]
+    # Proceso
+    ordenacion_burbuja(lista)
+    # Salida
+    print("Lista ordenada: ", lista)
--- test/test_algoritmo.py
+++ test/test_algoritmo.py
@@ -1,2 +1,5 @@
+import pytest
+from src.algoritmo_burbuja import ordenacion_burbuja
 
-
+def test_ordenacion_burbuja():
+    assert ordenacion_burbuja([8, 3, 1, 19, 14]) == [1, 3, 8, 14, 19]
